ba658d93860ce48fd18c975c10e96fc45396ae83
Chuangbo Tong <tongchuangbo@cowell-100-64-34-82.resnet.ucsc.edu>
2019-04-30 23:21:27 -0700

    added 2 buttons and one activity, didn't do much

--- app/src/main/java/com/btanhuecrng13ctong3/easyliving/MainActivity.java
+++ app/src/main/java/com/btanhuecrng13ctong3/easyliving/MainActivity.java
@@ -1,7 +1,10 @@
 package com.btanhuecrng13ctong3.easyliving;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login = (Button) findViewById(R.id.login);
+        final Button myPage = (Button) findViewById(R.id.MyPage);
+        myPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), mypage.class);
+                startActivity(intent);
+            }
+        });
     }
 }

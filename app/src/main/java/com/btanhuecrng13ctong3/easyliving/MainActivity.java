76cec4c7eb4e9da99c252cf539d57a681204d13c
Brix Tanhueco <btanhuec@ucsc.edu>
2019-05-03 18:45:54 -0700

    made login with no firebase

--- app/src/main/java/com/btanhuecrng13ctong3/easyliving/MainActivity.java
+++ app/src/main/java/com/btanhuecrng13ctong3/easyliving/MainActivity.java
@@ -3,23 +3,47 @@ package com.btanhuecrng13ctong3.easyliving;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userEmail;
+    EditText userPW;
+    Button login;
+    Button newAccount;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login = (Button) findViewById(R.id.login);
-        final Button myPage = (Button) findViewById(R.id.MyPage);
-        myPage.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+        userEmail = findViewById(R.id.emailText);
+        userPW = findViewById(R.id.passwordText);
+        login = findViewById(R.id.loginBtn);
+        newAccount = findViewById(R.id.newAccountBtn);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        newAccount.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), mypage.class);
+                Intent intent = new Intent(getApplicationContext(), newAccount.class);
                 startActivity(intent);
+                Log.d("Tag","newAccountPage");
             }
         });
+
+        //final Button myPage = (Button) findViewById(R.id.MyPage);
+        //myPage.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+            //    Intent intent = new Intent(getApplicationContext(), mypage.class);
+             //   startActivity(intent);
+            //}
+        //});
+
+
     }
 }

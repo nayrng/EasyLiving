d633a783388ba1f34ee7b610920f0a9ae578721b
Brix Tanhueco <btanhuec@ucsc.edu>
2019-05-03 20:11:45 -0700

    some security things for creating an account

--- app/src/main/java/com/btanhuecrng13ctong3/easyliving/newAccount.java
+++ app/src/main/java/com/btanhuecrng13ctong3/easyliving/newAccount.java
@@ -1,14 +1,24 @@
 package com.btanhuecrng13ctong3.easyliving;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class newAccount extends AppCompatActivity {
+
     EditText userEmail;
     EditText userPW;
     Button registerAccount;
@@ -16,6 +26,7 @@ public class newAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
         userEmail = findViewById(R.id.emailText2);
@@ -25,7 +36,38 @@ public class newAccount extends AppCompatActivity {
         registerAccount.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                firebaseAuth.createUserWithEmailAndPassword(userEmail.getText().toString(), userPW.getText().toString());
+                if(TextUtils.isEmpty(userEmail.getText())){
+                    Toast.makeText(newAccount.this, "Please enter a valid email", Toast.LENGTH_SHORT).show(); //displays notification
+                    return;//prevents the app from running any further
+                }
+                if(TextUtils.isEmpty(userPW.getText())){
+                    Toast.makeText(newAccount.this, "Please enter a valid password", Toast.LENGTH_SHORT).show(); //displays notification
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(userEmail.getText().toString(), userPW.getText().toString())
+                        .addOnCompleteListener(newAccount.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    //Log.d(TAG, "createUserWithEmail:success");
+                                    //FirebaseUser user = firebaseAuth.getCurrentUser(); <--Firebase Code IDK what it does
+                                    //updateUI(user);<--Firebase Code IDK what it does
+                                    Toast.makeText(newAccount.this, "Registration success!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), LandingActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(newAccount.this, "Registration failed, please try again.", Toast.LENGTH_SHORT).show();
+                                    //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",<--Firebase Code IDK what it does
+                                     //       Toast.LENGTH_SHORT).show();<--Firebase Code IDK what it does
+                                    //updateUI(null);<--Firebase Code IDK what it does
+                                }
+                                // ...
+                            }
+                        });
             }
         });
     }

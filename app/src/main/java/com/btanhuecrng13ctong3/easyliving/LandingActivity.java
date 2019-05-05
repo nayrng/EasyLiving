f7f168b41e25f9f04044ed3732724dae8d2d3d5d
nayrng <username@users.noreply.github.com>
2019-05-04 20:44:25 -0700

    linked chores button to chores activity

--- app/src/main/java/com/btanhuecrng13ctong3/easyliving/LandingActivity.java
+++ app/src/main/java/com/btanhuecrng13ctong3/easyliving/LandingActivity.java
@@ -1,7 +1,10 @@
 package com.btanhuecrng13ctong3.easyliving;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        Button chores =(Button) findViewById(R.id.buttonChores);
+        chores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ChoresActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+
 }
